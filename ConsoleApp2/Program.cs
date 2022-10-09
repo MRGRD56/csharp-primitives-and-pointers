c2674d8c2a0dfa2589ec9d342425b225bbd73f3a
k.golikov <kgolikov@37bytes.com>
2022-10-09 10:52:59 +0500

    Initial commit

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+unsafe
+{
+    Console.WriteLine("Hello, World!");
+
+    int number1 = 24;
+    long number2 = long.MaxValue;
+    byte number3 = (byte) number2;
+    byte number4 = number3;
+
+    var n1Ptr = &number1;
+    var n2Ptr = &number2;
+    var n3Ptr = &number3;
+    var n4Ptr = &number4;
+
+    *n3Ptr = 4;
+}
